8b7eaf940da47e4f11c120635d1a4562591fdb53
Giles Thompson <giles@papaiti.com>
2020-03-03 12:00:19 +1300

    IndexRoute: added the ability to filter by tags

--- app/models/question.js
+++ app/models/question.js
@@ -1,9 +1,14 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   user: DS.belongsTo('user'),
   title: DS.attr('string'),
   description: DS.attr('string'),
   tags: DS.attr('string'),
-  views: DS.attr('number')
+  views: DS.attr('number'),
+
+  tagsArray: computed('tags', function() {
+    return this.get('tags').split(',');
+  })
 });
