2f8baff6f1ba4071b90d25eb68fc9762822f1af7
Giles Thompson <giles@papaiti.com>
2020-03-02 13:09:30 +1300

    QuestionsRoute: load questions on the index route
    Renamed index route from "dashboard" to "questions".
    Allow questions route to be visited by unauthenticated users.
    Display loaded questions on questions route.

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import { inject } from "@ember/service";
 import ApplicationRouteMixin from "ember-simple-auth/mixins/application-route-mixin";
 
 export default Route.extend(ApplicationRouteMixin, {
-  routeAfterAuthentication: "dashboard",
+  routeAfterAuthentication: "questions",
   session: inject(),
   currentSession: inject(),
   // routeAfterAuthentication: null, // Don't trigger transition after authenticate()
