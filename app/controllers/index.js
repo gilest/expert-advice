da6500cbd61c4024ddfeb617ecb044cab601ac97
Giles Thompson <giles@papaiti.com>
2020-03-03 12:00:19 +1300

    IndexRoute: added the ability to filter by tags

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,27 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { debounce } from '@ember/runloop';
 
 export default Controller.extend({
   session: service(),
 
-  queryParams: ["page"],
-  page: 1
+  queryParams: ['page', 'filter'],
+  page: 1,
+  filter: '',
+
+  initialSearchValue: '',
+
+  actions: {
+    enteredRoute() {
+      this.set('initialSearchValue', this.get('filter'));
+    },
+    filter(enteredText) {
+      debounce(() => {
+        this.setProperties({
+          filter: enteredText,
+          page: 1
+        })
+      }, 100);
+    }
+  }
 });
