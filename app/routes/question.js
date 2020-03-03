77f2a6500e100248ced7b9316c80b0e318d1d3aa
Giles Thompson <giles@papaiti.com>
2020-03-03 16:43:26 +1300

    QuestionRoute: record question views

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,8 +1,21 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import $ from 'jquery';
 
 export default Route.extend(AuthenticatedRouteMixin, {
   model(params) {
     return this.store.findRecord('question', params.slug);
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    const questionAdapter = this.store.adapterFor('questions');
+
+    $.ajax({
+      type: 'PATCH',
+      url: `${questionAdapter.urlForCreateRecord('question')}/record_view`,
+      contentType: 'application/json',
+      data: JSON.stringify({ id: model.get('id') })
+    });
   }
 });
